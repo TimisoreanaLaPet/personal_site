41fe26ecbabffa704dbe0b658d87befabbc6bf19
cristina <neacsucristinamihaela@yahoo.com>
2025-07-31 15:42:38 +0200

    .

--- main/models.py
+++ main/models.py
@@ -11,6 +11,3 @@ class Subscriber(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     subscribed_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.salutation} {self.name}"
